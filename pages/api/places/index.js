e50848c694820d7f529ca41669a29272e0ca6a3c
Stephan Lippert <stephman1984@gmail.com>
2023-09-28 22:10:49 +0200

    fixed

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,26 +1,19 @@
-import dbConnect from "@/db/dbConnect";
 import Place from "@/db/models/Place";
-
+import dbConnect from "@/db/dbConnect";
 export default async function handler(request, response) {
   await dbConnect();
-
   if (request.method === "GET") {
     const places = await Place.find();
     return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "Methos not allowed!!!" });
   }
-
   try {
     if (request.method === "POST") {
       const places = request.body;
       await Place.create(places);
-      response.status(201).json({ status: "You're Place was created." });
+      response.status(201).json({ status: "Places created." });
     }
   } catch (error) {
-    console.error(error);
-    response
-      .status(400)
-      .json({ status: `Something went wrong: ${error.message}` });
+    console.log(error);
+    response.status(400).json({ error: error.message });
   }
 }
